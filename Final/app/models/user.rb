b53f82e28346c910095a5bbc195e76dc1b0ec624
jpereira23 <jpereira1@mail.csuchico.edu>
2015-12-02 22:27:24 -0800

    Lost a lot of stuff so I really need to commit

--- Final/app/models/user.rb
+++ Final/app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :tag_users
+
+  validates_presence_of :avatar 
+  validates_integrity_of :avatar 
+  validates_processing_of :avatar
 end
--- Final/config/routes.rb
+++ Final/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "articles#index"
 
+  get 'user' => 'articles#user'
   resources :articles do
     resources :tag_articles, shallow: true
 	member do
